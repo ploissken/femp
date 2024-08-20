40f024a41d072e64bf46e16b458b8ecde255be96
rodurico <ploissken@gmail.com>
2024-08-20 18:11:31 -0300

    add about img

--- src/components/sections/About.vue
+++ src/components/sections/About.vue
@@ -7,6 +7,9 @@
           <p class="text-justify">{{ $t("about.company_focus") }}</p>
           <p class="text-justify pt-2">{{ $t("about.company_plus") }}</p>
         </v-col>
+        <v-col cols="12" md="6" class="d-flex justify-center align-center">
+          <v-img src="../../assets/about.png" />
+        </v-col>
       </v-row>
       <v-row class="my-16">
         <v-col cols="12" md="3">
