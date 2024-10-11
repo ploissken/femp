4fd78dee42d88f57c49739b1c4771d775dcb1605
rodurico <ploissken@gmail.com>
2024-10-11 14:13:10 -0300

    Revert "update env usage"
    This reverts commit fd827ca717909a9967c76b4d5ae0c4016f0fd429.

--- src/components/FempMap.vue
+++ src/components/FempMap.vue
@@ -15,7 +15,8 @@ import { GoogleMap, AdvancedMarker } from "vue3-google-map";
 
 const gMapsApiKey = process.env.VUE_APP_GMAPS_API;
 const center = { lat: -23.305877, lng: -45.9635352 };
-
+console.log("gMapsApiKey", gMapsApiKey);
+console.log("process.env", process.env);
 const onMarkerClick = () => {
   window.open(
     "https://www.google.com/maps/place/R.+Dr.+Pomp%C3%ADlio+Mercadante,+398+-+Centro,+Jacare%C3%AD+-+SP,+12308-510/@-23.3059018,-45.9639272,17z/data=!3m1!4b1!4m6!3m5!1s0x94cdcb72e9a29847:0xf842f7ce0d6d1835!8m2!3d-23.3059049!4d-45.9623222!16s%2Fg%2F11c259f_77?entry=ttu&g_ep=EgoyMDI0MDkxOC4xIKXMDSoASAFQAw%3D%3D",
